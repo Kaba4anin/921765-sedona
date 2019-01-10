522261c427e906bd806fe4954ec4a878d7c5ef2a
Kaba4anin <stas_kabanov@mail.ru>
2019-01-10 22:18:28 +0300

    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 var link = document.querySelector(".form-button");
 var popup = document.querySelector(".search-form");
 
+var date_arrival = popup.querySelector("[name=date-arrival]");
+var date_of_departure = popup.querySelector("[name=date-of-departure]");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("search-form-show");
+});
+
+popup.addEventListener("submit", function(evt){
+  if (!date_arrival.value || !date_of_departure.value)
+  {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
